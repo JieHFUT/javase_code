4825281be4a83948a96c707ce41b888d328e65f4
jieHFUT <3492779706@qq.com>
2022-07-17 23:16:26 +0800

    尝试提交

--- practice_01/src/practice_01.java
+++ practice_01/src/practice_01.java
@@ -1,7 +1,14 @@
 public class practice_01 {
     public static void main(String[] args) {
+        int a = 10;//不初始化会进行报错
+        System.out.println(a);
+    }
+    public static void main1(String[] args) {
         //sout_输出
         //psvm_main
         System.out.println("Hello World");
+        boolean flg = true;
+        System.out.println(flg);//flg.sout_快捷键
+
     }
 }

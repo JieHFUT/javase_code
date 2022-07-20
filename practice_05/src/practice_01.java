c74a7cb8b325d5387d47bfb29ff5ea5cc87aeadb
jieHFUT <3492779706@qq.com>
2022-07-20 14:19:29 +0800

    方法

--- practice_05/src/practice_01.java
+++ practice_05/src/practice_01.java
@@ -8,8 +8,51 @@ import java.util.Scanner;
  * Time: 14:20
  */
 public class practice_01 {
+    //给一个正整数N；判断这个数字是不是2的K次方！不用求解K是多少。
+    public static boolean judgeNumber(int n){
+        int count = 0;
+        if(n != 0){
+            count++;
+            n = n & (n-1);
+        }
+        if(count == 1){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public static int countTheNumbertOfOne3(int n){
+        int count = 0;
+        while(n != 0){
+            n  = n & (n-1);
+            count++;
+        }
+        return count;
+    }
+    public static int countTheNumbertOfOne2(int n){
+        //不一定都需要移动32次
+        int count = 0;
+        if(n != 0){
+            for(int i = 0; i < 32; i++){
+                if(((n >>> i)&1) == 1) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static int countTheNumberOfOne1(int n){
+        //无论结果如何，都需要移动32次
+        int count = 0;
+        for(int i = 0; i < 32; i++){
+            if(((n >> i) & 1) == 1){
+                count++;
+            }
+        }
+        return count;
+    }
     //Math.sqrt();  Math.pow();  进入Math类 alt+7
-    public static void perfect(){
+    public static void perfectNumber(){
         //求位数
         for(int i = 0; i <= 10_000_000; i++){
             int j = i;
@@ -59,6 +102,30 @@ public class practice_01 {
         int year  = scanner.nextInt();
         boolean flg = isLeapYear(year);
 
-        perfect();
+        //perfectNumber();
+
+        System.out.println("请输入您要输入的数字：");
+        int num1 = scanner.nextInt();
+        int count1 = countTheNumberOfOne1(num1);
+        System.out.println("该数的二进制中1的个数为：" + count1);
+
+        System.out.println("请输入您要输入的数字：");
+        int num2 = scanner.nextInt();
+        int count2 = countTheNumberOfOne1(num2);
+        System.out.println("该数的二进制中1的个数为：" + count2);
+
+        System.out.println("请输入您要输入的数字：");
+        int num3 = scanner.nextInt();
+        int count3 = countTheNumberOfOne1(num3);
+        System.out.println("该数的二进制中1的个数为：" + count3);
+
+        System.out.println("请输入您要输入的数字：");
+        int num4 = scanner.nextInt();
+        boolean b = judgeNumber(num4);
+        if(b){
+            System.out.println("该数是2的K次方！");
+        }else {
+            System.out.println("该数不是2的K次方！");
+        }
     }
 }

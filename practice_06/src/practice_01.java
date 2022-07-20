f5d85bcc08328803f1a85efa80e4c41893823ba3
jieHFUT <3492779706@qq.com>
2022-07-21 00:02:34 +0800

    数组

--- practice_06/src/practice_01.java
+++ practice_06/src/practice_01.java
@@ -7,7 +7,27 @@
  */
 public class practice_01 {
     public static void main(String[] args) {
-        //function
+        //数组的学习
+        //中括号当中不能有任何的数字
+        int[] array1 = {1,2,3,4,5,6,7,8,9,10};
+        int[] array2 = new int[]{1,2,3,4,5,6,7,8,9,10};
+        System.out.println(array1.length);
+        System.out.println(array1[10]);//ArrayIndexOutOfBoundsException----与c不同，不允许数组越界
+
+
+        //动态初始化
+        int[] array3 = new int[10];//没有初始化的数组，默认全部都是 -> 0/null/false
+        array3[1] = 10;
+
+        //简写只有一次机会赋值机会
+        int array4[];
+        array4 = new int[]{1,2,3,4,5};
+        array4 = new int[]{5,6,7,8,9};
+
+        int[] array5 = {1,2,3,4,5};
+        //array5 = {2,3,4,5,6}; ------- 报错
+
+
 
     }
 }

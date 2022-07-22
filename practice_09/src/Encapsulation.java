ed9eec6a9f444cab5c7e67ef12a43a639b1b2f5b
jieHFUT <3492779706@qq.com>
2022-07-22 17:16:33 +0800

    数据/类和对象

---
+++ practice_09/src/Encapsulation.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-07-22
+ * Time: 15:50
+ */
+
+public class Encapsulation {
+    static boolean flg;
+
+    public static void main(String[] args) {
+        System.out.println(flg);
+    }
+
+
+
+}
+
+

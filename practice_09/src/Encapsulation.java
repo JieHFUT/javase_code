eeb678d3be03080c14028df523e75e3f9837d9c0
jieHFUT <3492779706@qq.com>
2022-07-22 17:24:59 +0800

    封装

--- practice_09/src/Encapsulation.java
+++ practice_09/src/Encapsulation.java
@@ -7,9 +7,13 @@
  */
 
 public class Encapsulation {
+    //在方法当中定义的变量是局部变量，而静态的变量属于类变量。随着类的加载而被创建
+    //而局部变量是调用该方法的时候，才创建的。
     static boolean flg;
 
     public static void main(String[] args) {
+
+
         System.out.println(flg);
     }
 

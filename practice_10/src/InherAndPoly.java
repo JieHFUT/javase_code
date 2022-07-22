37f5b79015cdf4c1b77c2aca3573580af14d3871
jieHFUT <3492779706@qq.com>
2022-07-23 04:26:27 +0800

    继承和多态

---
+++ practice_10/src/InherAndPoly.java
@@ -0,0 +1,29 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-07-23
+ * Time: 4:25
+ */
+public class InherAndPoly {
+    //继承和多态
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+    }
+
+
+}

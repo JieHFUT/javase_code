02a63f06a1c5ddaa93ae5076627e2793315f71d1
jieHFUT <3492779706@qq.com>
2022-07-24 02:33:12 +0800

    多态加抽象接口

---
+++ practice_12/src/poly_and_interface/Poly.java
@@ -0,0 +1,21 @@
+package poly_and_interface;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: JIE
+ * Date: 2022-07-24
+ * Time: 2:28
+ */
+
+
+public class Poly {
+    //多态的优缺点
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
